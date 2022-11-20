6646aafa28eafb56c8b908727fb814ba76986465
phongduong-dp <duonghongphong.dp@gmail.com>
2022-11-20 17:09:27 +0700

    Adding API for cities

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoAPIOption = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "57d3e00f4cmshf71fd78eac96cb6p1a77e0jsn8f98f45dc23d",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
