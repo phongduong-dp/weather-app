f460459bdf34506b0dbeec8d64a6d26a108c9e8e
phongduong-dp <duonghongphong.dp@gmail.com>
2022-11-21 23:13:04 +0700

    Handle weather fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Search from "./components/search/Search";
 import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
 import "./App.css";
+import { WEATHER_API_KEY, WEATHER_API_URL, FORECAST_API_URL } from "./api";
 
 function App() {
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, long] = searchData.value.split(" ");
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${long}&exclude={part}&appid=${WEATHER_API_KEY}`
+    );
+    const forecastWeatherFetch = fetch(
+      `${FORECAST_API_URL}/forecast?lat=${lat}&lon=${long}&appid=${WEATHER_API_KEY}`
+    );
   };
   return (
     <div className="container">
