e233daf6d85fe7a1ec0ced26e3bb5f2fa401bc3f
phongduong-dp <duonghongphong.dp@gmail.com>
2022-11-23 23:50:10 +0700

    Update current weather with dynamic data

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -36,11 +36,15 @@ function CurrentWeather(props) {
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
-            <span className="parameter-value">15%</span>
+            <span className="parameter-value">{`${Math.round(
+              props.data.main.humidity
+            )}%`}</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Pressure</span>
-            <span className="parameter-value">15 hPa</span>
+            <span className="parameter-value">{`${Math.round(
+              props.data.main.pressure
+            )} hPa`}</span>
           </div>
         </div>
       </div>
