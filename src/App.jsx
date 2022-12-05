cd634ff3e4e833b475200017210a0f0d0851f019
phongduong-dp <duonghongphong.dp@gmail.com>
2022-12-05 22:58:53 +0700

    Adding details component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { useState } from "react";
 import ForeCast from "./components/ForeCast/ForeCast";
 import React from "react";
+import Details from "./components/Details/Details";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
@@ -12,10 +13,18 @@ function App() {
   const handleOnSearchChange = (searchData) => {
     const [lat, long] = searchData.value.split(" ");
     const currentWeatherFetch = fetch(
-      `${import.meta.env.VITE_WEATHER_API_URL}/weather?lat=${lat}&lon=${long}&exclude={part}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`
+      `${
+        import.meta.env.VITE_WEATHER_API_URL
+      }/weather?lat=${lat}&lon=${long}&exclude={part}&appid=${
+        import.meta.env.VITE_WEATHER_API_KEY
+      }&units=metric`
     );
     const forecastWeatherFetch = fetch(
-      `${import.meta.env.VITE_FORECAST_API_URL}/forecast?lat=${lat}&lon=${long}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`
+      `${
+        import.meta.env.VITE_FORECAST_API_URL
+      }/forecast?lat=${lat}&lon=${long}&appid=${
+        import.meta.env.VITE_WEATHER_API_KEY
+      }&units=metric`
     );
     Promise.all([currentWeatherFetch, forecastWeatherFetch])
       .then(async (response) => {
@@ -30,6 +39,7 @@ function App() {
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <Details data={currentWeather} />}
       {currentWeather && <CurrentWeather data={currentWeather} />}
       {forecast && <ForeCast data={forecast} />}
     </div>
